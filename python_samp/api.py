b73040536ae17eba0c610f67a05a891efc3fa7fa
Vladimir <60815271+rmdlv@users.noreply.github.com>
2021-10-01 23:48:59 +0800

    0.1.0
    send_cmd and get_coordinates implementation

--- python_samp/api.py
+++ python_samp/api.py
@@ -6,14 +6,14 @@ class API:
     def __init__(self, samp: SAMP) -> None:
         self.samp = samp
 
-    def getUsername(self) -> str:
+    def get_username(self) -> str:
         """
         Get local player username
         """
         username = self.samp.process.read_string(self.samp.module + ADDR_SAMP_USERNAME)
         return username
 
-    def sendChat(self, message: str) -> None:
+    def send_chat(self, message: str) -> None:
         """
         Send message to chat
         """
@@ -21,3 +21,21 @@ class API:
         self.samp.process.write_string(address, message)
         self.samp.process.start_thread(self.samp.module + FUNC_SAMP_SENDSAY, address)
         self.samp.process.free(address)
+
+    def send_cmd(self, cmd: str) -> None:
+        """
+        Send command to server
+        """
+        address = self.samp.process.allocate(len(cmd))
+        self.samp.process.write_string(address, cmd)
+        self.samp.process.start_thread(self.samp.module + FUNC_SAMP_SENDCMD, address)
+        self.samp.process.free(address)
+
+    def get_coordinates(self) -> tuple:
+        """
+        Get local player coordinates
+        """
+        x = self.samp.process.read_float(ADDR_POSITION_X)
+        y = self.samp.process.read_float(ADDR_POSITION_Y)
+        z = self.samp.process.read_float(ADDR_POSITION_Z)
+        return (x, y, z)
