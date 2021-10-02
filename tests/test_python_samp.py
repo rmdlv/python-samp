60d0e85b736a1911e8de600ef471cdb06b08a5ee
Vladimir <60815271+rmdlv@users.noreply.github.com>
2021-10-02 15:37:10 +0800

    tests improvements

--- tests/test_python_samp.py
+++ tests/test_python_samp.py
@@ -1,8 +1,14 @@
-from python_samp import SAMP, API
+from python_samp import SAMP, API, Misc
+
+samp = SAMP()
+api = API(samp)
+misc = Misc(samp)
 
 
-def test_username():
-    samp = SAMP()
-    api = API(samp)
+def test_get_username():
     username = api.get_username()
     assert username == "Python_Samp"
+
+
+def test_get_jetpack():
+    misc.get_jetpack()
