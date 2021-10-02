b2c96f195c1b1b7d99947c10458624365b15557f
Vladimir <60815271+rmdlv@users.noreply.github.com>
2021-10-03 02:10:32 +0800

    removed wildcard imports, removed useless comments, fixed ignore files

--- python_samp/api.py
+++ python_samp/api.py
@@ -1,4 +1,16 @@
-from .memory import *
+from .memory_constants import (
+    ADDR_SAMP_USERNAME,
+    FUNC_SAMP_SENDSAY,
+    FUNC_SAMP_SENDCMD,
+    ADDR_POSITION_X,
+    ADDR_POSITION_Y,
+    ADDR_POSITION_Z,
+    ADDR_SET_POSITION,
+    ADDR_SET_POSITION_OFFSET,
+    ADDR_SET_POSITION_X_OFFSET,
+    ADDR_SET_POSITION_Y_OFFSET,
+    ADDR_SET_POSITION_Z_OFFSET,
+)
 
 from .samp import SAMP
 
@@ -8,43 +20,28 @@ class API:
         self.samp = samp
 
     def get_username(self) -> str:
-        """
-        Get local player username
-        """
         username = self.samp.process.read_string(self.samp.module + ADDR_SAMP_USERNAME)
         return username
 
     def send_chat(self, message: str) -> None:
-        """
-        Send message to chat
-        """
         address = self.samp.process.allocate(len(message))
         self.samp.process.write_string(address, message)
         self.samp.process.start_thread(self.samp.module + FUNC_SAMP_SENDSAY, address)
         self.samp.process.free(address)
 
     def send_cmd(self, cmd: str) -> None:
-        """
-        Send command to server
-        """
         address = self.samp.process.allocate(len(cmd))
         self.samp.process.write_string(address, cmd)
         self.samp.process.start_thread(self.samp.module + FUNC_SAMP_SENDCMD, address)
         self.samp.process.free(address)
 
     def get_coordinates(self) -> float:
-        """
-        Get local player coordinates
-        """
         x = self.samp.process.read_float(ADDR_POSITION_X)
         y = self.samp.process.read_float(ADDR_POSITION_Y)
         z = self.samp.process.read_float(ADDR_POSITION_Z)
         return x, y, z
 
     def set_coordinates(self, x: float, y: float, z: float) -> None:
-        """
-        Set local player coordninates
-        """
         address = self.samp.process.read_int(
             self.samp.process.read_int(ADDR_SET_POSITION) + ADDR_SET_POSITION_OFFSET
         )
--- python_samp/misc.py
+++ python_samp/misc.py
@@ -1,6 +1,11 @@
 import time
 
-from .memory import *
+from .memory_constants import (
+    FUNC_CHEATS_JETPACK,
+    ADDR_GAME_SPEED,
+    ADDR_GAME_GRAVITY,
+    ADDR_HUD_MONEY,
+)
 
 from .samp import SAMP
 from .api import API
@@ -12,56 +17,32 @@ class Misc:
         self.api = API(self.samp)
 
     def get_jetpack(self) -> None:
-        """
-        Give jetpack to local player
-        """
         self.samp.process.start_thread(FUNC_CHEATS_JETPACK)
 
     def set_game_speed(self, speed: float) -> None:
-        """
-        Set local game speed
-        """
         self.samp.process.write_float(ADDR_GAME_SPEED, speed)
 
     def get_game_speed(self) -> float:
-        """
-        Get local game speed
-        """
         speed = self.samp.process.read_float(ADDR_GAME_SPEED)
         return speed
 
     def set_world_gravity(self, gravity: float) -> None:
-        """
-        Set local world gravity
-        """
         self.samp.process.write_float(ADDR_GAME_GRAVITY, gravity)
 
     def get_world_gravity(self) -> float:
-        """
-        Get local world gravity
-        """
         gravity = self.samp.process.read_float(ADDR_GAME_GRAVITY)
         return gravity
 
     def set_player_money(self, money: int) -> None:
-        """
-        Set local player money
-        """
         self.samp.process.write_int(ADDR_HUD_MONEY, money)
 
     def get_player_money(self) -> int:
-        """
-        Get local player money
-        """
         money = self.samp.process.read_int(ADDR_HUD_MONEY)
         return money
 
     def coordmaster(
         self, x: float, y: float, z: float, sections: int = 50, sleep: int = 0.25
     ) -> None:
-        """
-        Sectional teleport to coordniates
-        """
         _x, _y, _z = self.api.get_coordinates()
         sx = (x - _x) / sections
         sy = (y - _y) / sections
